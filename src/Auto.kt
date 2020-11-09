43f06e3c05c7274f10822693f3ce09a9f9206235
Programador Novato <info@programadornovato.com>
2020-11-09 05:08:58 -0600

    Llamar nuestras clases desde otros archivos en Kotlin  (Curso de Kotlin desde cero [56]) Video: https://youtu.be/NupmHigNWa0

---
+++ src/Auto.kt
@@ -0,0 +1,33 @@
+class Auto{
+    private var color=""
+    private var modelo=0
+    private var marca=""
+    private var acceso=false
+    public fun meterLlave(llave:String){
+        if(llave=="123456"){
+            acceso=true
+            println("Tienes acceso al auto")
+        }else{
+            acceso=false
+            println("No tienes acceso al auto (sonar alarma)")
+        }
+    }
+    public fun mando(accion:String){
+        if(acceso==true){
+            if(accion=="encender"){
+                enciende()
+            }
+        }else{
+            println("No tienes acceso al auto (sonar alarma)")
+        }
+    }
+    private fun enciende(){
+        println("Enciende el auto run run")
+    }
+    private fun acelera(){
+        println("El auto acelero 10Km/h mas")
+    }
+    private fun frena(){
+        println("El auto freno a 10Km/h menos")
+    }
+}
---
+++ src/main.kt
@@ -0,0 +1,5 @@
+fun main(args:Array<String>){
+    var vocho1=Auto()
+    vocho1.meterLlave("123456")
+    vocho1.mando("encender")
+}
