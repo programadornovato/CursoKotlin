9436c1cc54d06c55f21bab9c34898f90e9445cf3
Programador Novato <info@programadornovato.com>
2020-11-09 18:13:24 -0600

    Revert "Revert "POO Método Constructor en Kotlin (Curso de Kotlin desde cero [58])""
    This reverts commit cab3e60c34274d21c7345f3f06824286fa1893c1.

---
+++ src/Persona.kt
@@ -0,0 +1,16 @@
+class Persona(_nombre:String,_edad:Int) {
+    var nombre:String=_nombre
+    var edad:Int=_edad
+    /*
+    constructor(_nombre:String,_edad:Int){
+        nombre=_nombre
+        edad=_edad
+    }
+    */
+    /*
+    public fun inicializar(_nombre:String,_edad:Int){
+        nombre=_nombre
+        edad=_edad
+    }
+    */
+}
