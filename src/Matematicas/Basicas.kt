1d4fb7e9e01964f6c487c87818871e237f14b3b0
Programador Novato <info@programadornovato.com>
2020-11-10 01:16:38 -0600

    POO Crear paquetes y clases en Kotlin (Curso de Kotlin desde cero [60])
    Video:
    https://youtu.be/egfsaWs8vxE

---
+++ src/Matematicas/Basicas.kt
@@ -0,0 +1,17 @@
+package Matematicas
+class Basicas (_num1:Int,_num2:Int){
+    var num1=_num1
+    var num2=_num2
+    fun suma():Int{
+        return num1+num2
+    }
+    fun resta():Int{
+        return num1-num2
+    }
+    fun multiplicacion():Int{
+        return num1*num2
+    }
+    fun divicion():Int{
+        return num1/num2
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,16 +1,9 @@
 import javax.swing.JOptionPane
-
+import Matematicas.Basicas
 fun main(args:Array<String>){
-    /*
-    println()
-    println("Hola")
-    println(5)
-    println(5.5)
-    println(5 .. 5)
-    */
-    var v1=Vehiculo("Vocho","1990")
-    v1.acelerar()
-    var v2=Vehiculo("vochoman1")
-    v2.acelerar(4)
-
+    var mat=Basicas(1,2)
+    println("Suma=${mat.suma()}")
+    println("Resta=${mat.resta()}")
+    println("Multiplicacion=${mat.multiplicacion()}")
+    println("Divicion=${mat.divicion()}")
 }
