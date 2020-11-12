d04f8a2e6383fa1c77a201a6ca6f1ec90d36f379
Programador Novato <info@programadornovato.com>
2020-11-12 01:59:13 -0600

    Arreglos dinámicos con ArrayList en Kotlin (Curso de Kotlin desde cero [68])
    Video:
    https://youtu.be/0ECDmdXgzcY

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,30 @@
 import javax.swing.JOptionPane
 
 fun main(args:Array<String>){
-    var m=Motocicletas()
-    m.llantas()
-    var a=Autos()
-    a.llantas()
-    var c=Camiones()
-    c.llantas()
+    //listaNormal()
+    listaDinamica()
+}
+fun listaNormal(){
+    var numElementos=JOptionPane.showInputDialog("Humano coloca la cantidad de elementos de tu lista").toInt()
+    var listaEstatica= arrayOfNulls<Int>(numElementos)
+    for(i in 0 until numElementos){
+        listaEstatica[i]=JOptionPane.showInputDialog("Ingresa un numero").toInt()
+    }
+    println("Aqui esta tu pinche lista")
+    listaEstatica.forEach {
+        println(it)
+    }
+}
+fun listaDinamica(){
+    var listaDinamica=ArrayList<Int>()
+    var respuesta:Char
+    do {
+        listaDinamica.add(JOptionPane.showInputDialog("Ingresa un numero").toInt())
+        respuesta=JOptionPane.showInputDialog("Quieres continuar s/n")[0]
+    }while ( respuesta=='s' || respuesta=='S' )
+    println("Aqui esta tu pinche lista")
+    listaDinamica.forEach {
+        println(it)
+    }
+
 }
