eb91c8b636813fccf3ea9ea3b62e04db495c23f5
Programador Novato <info@programadornovato.com>
2020-11-12 21:31:21 -0600

    Interfaz gráfica con JavaX Swing en Kotlin (Curso de Kotlin desde cero [70])
    Video:
    https://youtu.be/IlPf2z-GhZc

---
+++ src/Ventana.kt
@@ -0,0 +1,9 @@
+import java.awt.Dimension
+import javax.swing.JFrame
+
+class Ventana:JFrame(){
+    init {
+        var d=Dimension(500,500)
+        this.size=d
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,6 @@
 import javax.swing.JOptionPane
 
 fun main(args:Array<String>){
-    var v= arrayOfNulls<Vehiculo>(4)
-    v[0]=Vehiculo("XDSDF445","Nissan","2010")
-    v[1]=tipoTurismo("XDFGHJ777","Nissan","2018",5)
-    v[2]=tipoDeportivo("TTYYHH666","Nissan","2010",8)
-    v[3]=tipoFurgoneta("FDSEEW5666","Nissan","2015",20)
-    for (i in v.indices){
-        println(v[i]!!.entregarDatos())
-    }
+    var v=Ventana()
+    v.isVisible=true
 }
