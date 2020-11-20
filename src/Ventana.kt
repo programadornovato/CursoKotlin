cbc995250801851d07d84f1d1d537fdf7ce3acbf
Programador Novato <info@programadornovato.com>
2020-11-20 17:32:14 -0600

    Eventos del raton (MouseListener)(Curso de Kotlin desde cero [86])
    Video:
    https://youtu.be/OgtafQdt8fg

--- src/Ventana.kt
+++ src/Ventana.kt
@@ -1,6 +1,8 @@
 import java.awt.*
 import java.awt.event.ActionListener
 import java.awt.event.KeyEvent
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
 import javax.swing.*
 
 class Ventana(titulo:String?):JFrame(titulo){
@@ -11,7 +13,36 @@ class Ventana(titulo:String?):JFrame(titulo){
         defaultCloseOperation=WindowConstants.EXIT_ON_CLOSE
         this.contentPane.add(contenedor)
         contenedor.layout=null
-        evento()
+        //evento()
+        eventosRaton()
+    }
+    fun eventosRaton(){
+        var boton=JButton("Humano ponte a jugar con tu raton (¡¡¡El de la compu!!!)")
+        boton.setBounds(10,10,450,30)
+        contenedor.add(boton)
+
+        var caja=JTextField()
+        caja.setBounds(10,50,400,30)
+        contenedor.add(caja)
+
+        var l:MouseListener=object :MouseListener{
+            override fun mouseClicked(e: MouseEvent?) {
+                //caja.text="mouseClicked"
+            }
+            override fun mouseEntered(e: MouseEvent?) {
+                caja.text="mouseEntered"
+            }
+            override fun mouseExited(e: MouseEvent?) {
+                caja.text="mouseExited"
+            }
+            override fun mousePressed(e: MouseEvent?) {
+                caja.text="mousePressed"
+            }
+            override fun mouseReleased(e: MouseEvent?) {
+                caja.text="mouseReleased"
+            }
+        }
+        boton.addMouseListener(l)
     }
     fun evento(){
         var boton=JButton("Humano coloca tu miserable nombre en el cuadro de texto y da click aqui")
