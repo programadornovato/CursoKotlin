ecf233047a5eb06b2b7ea925c8178330a0891141
Programador Novato <info@programadornovato.com>
2020-11-23 02:32:17 -0600

    Mover una imagen en Kotlin(Java)🐈‍ (Curso de Kotlin desde cero [92])
    Video:
    https://youtu.be/7GkkwKgqkyk

--- src/Ventana.kt
+++ src/Ventana.kt
@@ -1,51 +1,46 @@
+import java.awt.Color
 import java.awt.Dimension
-import java.awt.Font
+import java.awt.Image
+import java.awt.event.ActionListener
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
 import javax.swing.*
 
+
 class Ventana ():JFrame(){
     var contenedor=JPanel()
-    var etiquetaPide=JLabel("¿Quieres ser mi novi@?")
-    var etiquetaAcepta=JLabel("¡¡Sabia que caerias en mis brazos!!")
-    var botonSi=JButton("Si")
-    var botonNo=JButton("No")
+    var etiqueta=JLabel()
     init {
-        var d=Dimension(500,350)
+        var d=Dimension(1000,350)
         this.size=d
         defaultCloseOperation=WindowConstants.EXIT_ON_CLOSE
+        contenedor.background= Color.white
         this.contentPane.add(contenedor)
         contenedor.layout=null
-        contenido()
-        eventos()
+        var logo=ImageIcon("src/images/neko.gif")
+        var logoMin=ImageIcon(logo.image.getScaledInstance(300,200, Image.SCALE_DEFAULT))
+        etiqueta.icon=logoMin
+        etiqueta.setBounds(10,90,400,300)
+        contenedor.add(etiqueta)
+        boton()
     }
-    fun eventos(){
-        eventoClickSi()
-        eventoEntraNo()
-    }
-    fun eventoEntraNo(){
-        var l:MouseListener=object :MouseListener{
+    fun boton(){
+        var boton=JButton("Inicia")
+        var i=0
+        var l:MouseListener=object:MouseListener{
             override fun mouseClicked(e: MouseEvent?) {
-            }
-            override fun mouseEntered(e: MouseEvent?) {
-                var xRandom=(100..400).random()
-                var yRandom=(30..270).random()
-                botonNo.setLocation(xRandom,yRandom)
-                println("xRandom=$xRandom,yRandom=$yRandom")
-            }
-            override fun mouseExited(e: MouseEvent?) {
-            }
-            override fun mousePressed(e: MouseEvent?) {
-            }
-            override fun mouseReleased(e: MouseEvent?) {
-            }
-        }
-        botonNo.addMouseListener(l)
-    }
-    fun eventoClickSi(){
-        var l:MouseListener=object :MouseListener{
-            override fun mouseClicked(e: MouseEvent?) {
-                etiquetaAcepta.isVisible=true
+                val timer = Timer(40, ActionListener {
+                    etiqueta.setLocation(i,90)
+                    repaint()
+                    i=i+10
+                    if(i>=800){
+                        i=800
+                    }
+                    println("i=$i")
+                })
+                timer.isRepeats = true
+                timer.isCoalesce = true
+                timer.start()
             }
             override fun mouseEntered(e: MouseEvent?) {
             }
@@ -56,21 +51,8 @@ class Ventana ():JFrame(){
             override fun mouseReleased(e: MouseEvent?) {
             }
         }
-        botonSi.addMouseListener(l)
-    }
-    fun contenido(){
-        etiquetaPide.setBounds(120,10,300,30)
-        etiquetaPide.font=Font(Font.MONOSPACED,Font.ITALIC,20)
-        contenedor.add(etiquetaPide)
-
-        botonNo.setBounds(100,50,100,30)
-        contenedor.add(botonNo)
-        botonSi.setBounds(300,50,100,30)
-        contenedor.add(botonSi)
-
-        etiquetaAcepta.setBounds(1,250,450,30)
-        etiquetaAcepta.font=Font(Font.MONOSPACED,Font.ITALIC,20)
-        contenedor.add(etiquetaAcepta)
-        etiquetaAcepta.isVisible=false
+        boton.setBounds(10,5,100,20)
+        contenedor.add(boton)
+        boton.addMouseListener(l)
     }
 }
